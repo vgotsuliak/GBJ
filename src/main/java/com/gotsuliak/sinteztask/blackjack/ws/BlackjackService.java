9a1e10c75d06529dced7b29e0a31bb0b9cdcbed0
Viacheslav Gotsuliak <Vjacheslav.Gotsuljak@privatbank.ua>
2014-07-15 18:50:57 +0300

    BlackjackService add method deposit

--- src/main/java/com/gotsuliak/sinteztask/blackjack/ws/BlackjackService.java
+++ src/main/java/com/gotsuliak/sinteztask/blackjack/ws/BlackjackService.java
@@ -2,6 +2,7 @@ package com.gotsuliak.sinteztask.blackjack.ws;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 @Path("/blackjack")
@@ -12,6 +13,13 @@ public class BlackjackService {
     @Produces("text/plain")
     public String start() {
         return "someCoolWalletID";
+    }
+
+    @GET
+    @Path("/deposit/{sum}")
+    @Produces("text/plain")
+    public String deposit(@PathParam("sum") Integer sum) {
+        return "deposit successfull + " + sum;
     }
 
 }
